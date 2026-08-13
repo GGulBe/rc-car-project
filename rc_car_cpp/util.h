30d834eec7b17ae553952b4d3409b18eaf51f8f3
KIMWOOSEOK <woosk918@gmail.com>
2026-08-13 14:29:44 +0900

    draw 삭제

--- rc_car_cpp/util.h
+++ rc_car_cpp/util.h
@@ -8,5 +8,4 @@
 std::runtime_error systemError(const std::string& message);
 std::string makePipeline(int width, int height, int fps, double speedSetting, double driveCommand, double steeringAngle, double cameraPan, double cameraTilt, double fps,Bno055::Tilt tilt, UartDevice::gpsdata gpsdata);
 std::string makePhotoFilename();
-void drawStatus(cv::Mat& frame, double speedSetting, double driveCommand, double steeringAngle, double cameraPan, double cameraTilt, double fps, Bno055::Tilt tilt, UartDevice::gpsdata gpsdata);
 

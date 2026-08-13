d4c928bcc5f1bb95a1da482086d0ac0f63579ae1
KIMWOOSEOK <woosk918@gmail.com>
2026-08-13 10:14:12 +0900

    에러 수정

--- rc_car_cpp/util.cpp
+++ rc_car_cpp/util.cpp
@@ -32,7 +32,7 @@ std::string makePhotoFilename() {
     return name.str();
 }
 
-void drawStatus(cv::Mat& frame, double speedSetting, double driveCommand, double steeringAngle, double cameraPan, double cameraTilt, double fps, std::ostringstream imuline, std::ostringstream gpsline) {
+void drawStatus(cv::Mat& frame, double speedSetting, double driveCommand, double steeringAngle, double cameraPan, double cameraTilt, double fps, std::ostringstream& imuline, std::ostringstream& gpsline) {
     std::ostringstream line1;
     line1 << std::fixed << std::setprecision(1) << "FPS " << fps << "  Speed " << speedSetting << "%  Drive " << driveCommand << "%";
     std::ostringstream line2;
